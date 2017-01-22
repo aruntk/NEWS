b38ffda7b24337ef05fc347dc6579bf93e97f595
aruntk <aruntknambiar@gmail.com>
2017-01-22 18:50:36 +0530

    webpack 2.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -9,30 +9,30 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'build.js',
   },
-  resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
-  },
+  // resolveLoader: {
+    // root: path.join(__dirname, 'node_modules'),
+  // },
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file',
+        loader: 'file-loader',
         query: {
           name: '[name].[ext]?[hash]'
         }
       },
       {
         test: /\.html$/,
-        loader: 'wc',
+        loader: 'babel-loader!wc-loader',
       },
       {
         test: /\.json$/,
-        loader: 'json'
+        loader: 'json-loader'
       }
     ]
   },
